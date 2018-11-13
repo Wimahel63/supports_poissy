baf3649e92162ac52a7bba9a1deb9d6c1f667c1a
Magali Piszczyglowa <magali.piszczyglowa@lepoles.com>
2018-11-13 17:13:47 +0100

    13/11

---
+++ mon_premier_site/js/fancy_box.js
@@ -0,0 +1,6 @@
+$('[data-fancybox]').fancybox({
+    loop: true,//si on fait effet loupe, alors les boutons apparaissent
+    buttons: ["zoom", "share", "slideShow", "fullScreen", "download", "thumbs", "close"],
+    animationEffect: "zoom-in-out"
+});
+//thumb correspond au double zoom
