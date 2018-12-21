c3f22be84a7e1a06269869d88c48e2bded5ccd8c
Magali Piszczyglowa <magali.piszczyglowa@lepoles.com>
2018-12-21 17:30:29 +0100

    21/12

---
+++ nodejs/movie-app-3 - Copie/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { MyMoviesPageModule } from '../pages/my-movies/my-movies.module';
+import { MovieDetailPageModule } from '../pages/movie-detail/movie-detail.module';
+import { MovieListPageModule } from '../pages/movie-list/movie-list.module';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    ListPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    MyMoviesPageModule,
+    MovieDetailPageModule,
+    MovieListPageModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    ListPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
